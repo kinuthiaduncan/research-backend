99d7b700d0419ebe93043c3c88bd56a65da16a0e
Kinuthia <kinuthiaduncan@gmail.com>
2019-06-06 14:55:54 +0300

    Google trends api done

---
+++ app/controllers/CensorshipController.scala
@@ -0,0 +1,39 @@
+package controllers
+
+import java.net.{URI, URLEncoder}
+
+import akka.http.javadsl.model.HttpResponse
+import javax.inject.Inject
+import models.FocusGroupRepository
+import play.api.Configuration
+import play.api.libs.json.Json
+import scala.util.{Failure, Success}
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.io.BufferedSource
+
+class CensorshipController @Inject() (scc: SecuredControllerComponents) (repo: FocusGroupRepository)
+                                     (implicit ec: ExecutionContext, conf:Configuration) extends SecuredController(scc) {
+
+  def interestOverTime() = AuthenticatedAction.async(implicit request => {
+    val apiUrl: String = "http://nodebackend.test/google/interest_over_time"
+    val geo = request.body.asFormUrlEncoded.get("geo").head
+    val keyword = request.body.asFormUrlEncoded.get("keyword").head
+    val startTime = request.body.asFormUrlEncoded.get("startTime").head
+    val endTime = request.body.asFormUrlEncoded.get("endTime").head
+    val decodedUrl = apiUrl+"?keyword="+keyword+"&geo="+geo+"&startTime="+startTime+"&endTime="+endTime
+    val content: BufferedSource = scala.io.Source.fromURL(decodedUrl, "utf-8")
+    val data = scala.collection.mutable.Map[String, String]()
+    try {
+      content.getLines().foreach(item => {
+        data("results") = item
+      })
+    } catch {
+      case ioe: java.io.IOException => Failure(ioe)
+      case ste: java.net.SocketTimeoutException => Failure(ste)
+    }
+    Future(Ok(Json.obj("data" -> data)))
+  })
+
+  def get(url: String) = scala.io.Source.fromURL(url)
+}
